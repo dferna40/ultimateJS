47768f34bd926a460e986759e11237db0809ca11
Dferna40 <dferna40@gmail.com>
2024-12-27 21:17:57 +0100

    último ejercicio arrays

---
+++ 08-ejercicios/09.js
@@ -0,0 +1,11 @@
+const usuarios = [
+    {edad: 17, nombre: 'Nico', plan: 'premium'},
+    {edad: 13, nombre: 'David', plan: 'free'},
+    {edad: 32, nombre: 'Ana', plan: 'free'},
+];
+
+const users = [
+    {edad: 17, nombre: 'Juan', plan: 'premium'},
+    {edad: 13, nombre: 'Paco', plan: 'free'},
+    {edad: 32, nombre: 'Diego', plan: 'free'},
+];
